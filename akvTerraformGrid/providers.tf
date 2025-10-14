0b3bc6b0271c22bd733a99335b64b5bce2f6d741
Robert Moriarty <bobbymoriarty13@gmail.com>
2025-10-14 13:00:02 -0500

    Update providers.tf

--- akvTerraformGrid/providers.tf
+++ akvTerraformGrid/providers.tf
@@ -10,7 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # <<< Add these two lines >>>
-  subscription_id = "fc1f1d7c-bc1f-4378-933e-0e1ea09b5f4b"
-  tenant_id       = "fca47690-ab7d-413c-90e4-aafd92aac945"
+  
+  subscription_id = " "
+  tenant_id       = " "
+
 }
